b07d39396a71981bb5be4e52c0953b01f5ab71f6
jyw-atgithub <jenyuw@uci.edu>
2024-01-10 13:14:50 -0800

    add

--- assemblyQC.sh
+++ assemblyQC.sh
@@ -28,9 +28,22 @@ name2=$(basename $i ".racon.fasta")
 busco -f -i $i -o ${name2}-racon-busco -m genome -l actinopterygii_odb10 -c ${nT}
 done
 
+for i in `ls ${polishing}/C01.*.nextpolish.fasta.PolcaCorrected.fa`
+do
+name2=$(basename $i ".nextpolish.fasta.PolcaCorrected.fa")
+busco -f -i $i -o ${name2}-polca-busco -m genome -l actinopterygii_odb10 -c ${nT}
+done
+
+#The final patched and purged assembly
+busco -f -i /home/jenyuw/Fish-project/result/purge_dups/C01.fl.ca.nd.pd/purged.fa \
+-o pd2-busco -m genome -l actinopterygii_odb10 -c 30
 
 conda deactivate
 
+compleasm_out="/home/jenyuw/Fish-project/result/compleasm_out"
+#this only need to be run once
+#python3 /home/jenyuw/Software/compleasm_kit/compleasm.py download actinopterygii_odb10
 
-busco -f -i /home/jenyuw/Fish-project/result/purge_dups/C01.flye.canu.pd/purged.fa \
--o pd2-busco -m genome -l actinopterygii_odb10 -c 30
+python3 /home/jenyuw/Software/compleasm_kit/compleasm.py run  -a  /home/jenyuw/Fish-project/result/purge_dups/C01.fl.ca.nd.pd/purged.fa \
+-o ${compleasm_out}/C01_3_pdpa -t ${nT} \
+-l actinopterygii_odb10 -L "/home/jenyuw/Software/compleasm_kit/mb_downloads/"
