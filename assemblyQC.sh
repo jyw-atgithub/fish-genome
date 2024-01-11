000994b9abdf4ff3a742d8ea0b5de41fd1106f69
jenyuw <jenyuw@uci.edu>
2024-01-10 16:20:35 -0800

    merqury ok

--- assemblyQC.sh
+++ assemblyQC.sh
@@ -3,6 +3,10 @@ source ~/.bashrc
 
 assembly="/home/jenyuw/Fish-project/result/assembly"
 polishing="/home/jenyuw/Fish-project/result/polishing"
+trimmed="/home/jenyuw/Fish-project/result/trimmed"
+merqury_out="/home/jenyuw/Fish-project/result/merqury_out"
+purge_dups="/home/jenyuw/Fish-project/result/purge_dups"
+compleasm_out="/home/jenyuw/Fish-project/result/compleasm_out"
 nT=30
 
 conda activate busco
@@ -40,10 +44,25 @@ busco -f -i /home/jenyuw/Fish-project/result/purge_dups/C01.fl.ca.nd.pd/purged.f
 
 conda deactivate
 
-compleasm_out="/home/jenyuw/Fish-project/result/compleasm_out"
+
+##Compleasm
+
+
 #this only need to be run once
 #python3 /home/jenyuw/Software/compleasm_kit/compleasm.py download actinopterygii_odb10
 
-python3 /home/jenyuw/Software/compleasm_kit/compleasm.py run  -a  /home/jenyuw/Fish-project/result/purge_dups/C01.fl.ca.nd.pd/purged.fa \
+python3 /home/jenyuw/Software/compleasm_kit/compleasm.py run -a /home/jenyuw/Fish-project/result/purge_dups/C01.fl.ca.nd.pd/purged.fa \
 -o ${compleasm_out}/C01_3_pdpa -t ${nT} \
 -l actinopterygii_odb10 -L "/home/jenyuw/Software/compleasm_kit/mb_downloads/"
+
+##Merqury
+
+#sh /home/jenyuw/Software/merqury/best_k.sh 135000000
+#k=23.3272 ==> 23
+
+meryl k=23 count ${trimmed}/P96.trimmed.r*.fastq.gz output ${merqury_out}/P96.meryl
+
+cd /home/jenyuw/Fish-project/result/merqury_out
+##Do NOT use "sh ......"
+bash /home/jenyuw/Software/merqury/merqury.sh /home/jenyuw/Fish-project/result/merqury_out/P96.meryl \
+/home/jenyuw/Fish-project/result/purge_dups/C01.fl.ca.nd.pd/purged.fa C01_3_papd
---
+++ scaffold.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+scaffold="/home/jenyuw/Fish-project/result/scaffold"
+purge_dups="/home/jenyuw/Fish-project/result/purge_dups"
+
+#scaffolding and gap filling
