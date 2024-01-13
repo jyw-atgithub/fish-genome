3ec10e2b92f9bce14309d15d98485bbee5a2bfaa
jyw-atgithub <jenyuw@uci.edu>
2024-01-13 02:26:18 -0800

    scfd

--- annotating.sh
+++ annotating.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+repeat="/home/jenyuw/Fish-project/result/repeat"
+purge_dups="/home/jenyuw/Fish-project/result/purge_dups"
+
 ##repeat modeling
 #singularity build dfam-tetools.sif docker://dfam/tetools:latest
 #singularity run dfam-tetools.sif
 #singularity pull dfam-tetools-latest.sif docker://dfam/tetools:latest
 #singularity run dfam-tetools-latest.sif --> Then, it enters the interactive mode
 
+singularity exec /home/jenyuw/Software/dfam-tetools-latest.sif RepeatModeler -database \ -threads 30 
+
 ##repeat masking
 
 ##annotation
 
 singularity exec galba.sif galba.pl --genome=genome.fa --species=speciesname --prot_seq=proteins.fa --hints=hints.gff \
---species=sname --threads 30 --workingdir=/path/to/wd/ --crf 
+--species=sname --threads 30 --workingdir=/path/to/wd/ --crf  
--- scaffold.sh
+++ scaffold.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-scaffold="/home/jenyuw/Fish-project/result/scaffold"
+trimmed="/home/jenyuw/Fish-project/result/trimmed"
 purge_dups="/home/jenyuw/Fish-project/result/purge_dups"
+scaffold="/home/jenyuw/Fish-project/result/scaffold"
 
 #scaffolding and gap filling
+
+
+conda activate ntLink
+#because of too many dependencies, anaconda is more convenient
+nohup ntLink_rounds run_rounds_gaps target=${purge_dups}/C01.fl.ca.nd.pd/purged.fa reads=${trimmed}/C01.trimmed.fastq.gz rounds=4 \
+k=24 w=250 t=5 soft_mask=True > ${scaffold}/ntLink.out &
+#max thread is 5
+conda deactivate
+
+#For Linux 64, Open MPI is built with CUDA awareness but this support is disabled by default.
+#To enable it, please set the environment variable OMPI_MCA_opal_cuda_support=true before launching your MPI processes.
